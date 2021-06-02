913c11b1f531ed838c8263892a26576cb1483fd9
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-06-01 19:50:51 -0500

    file system and sync/async testing on read/write

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const http = require('http');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ---------------------------------------------- File System -------------------------------------------------------*/
+
+// // blocking code execution
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File write SUCCESS!')
+
+// // non-blocking code execution + callback hell
+// // tested error handler with change of file name to ...startttt.txt
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.error('ERROR!')
+
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3)
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('File has been written!!!!!!')
+//             })
+//         })
+//     })
+// })
+// console.log('Will read file at some point!') // this happens first while waiting on callback
