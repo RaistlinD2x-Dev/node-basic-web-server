397e14d03448bbd77a069e26ad7b985875ce6781
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-06-01 20:04:40 -0500

    setup webserver

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const http = require('http');
 
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server')
+})
 
-
-
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server is listening on port 8000')
+})
 
 
 
